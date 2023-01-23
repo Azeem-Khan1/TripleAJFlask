ab5085e3f9859c303ca67dca0518fa4496e7bbf9
Azeem-Khan1 <aalookid@gmail.com>
2023-01-22 18:16:16 -0800

    working database

---
+++ api/players.py
@@ -0,0 +1,61 @@
+from flask import Blueprint, request, jsonify
+from flask_restful import Api, Resource # used for REST API building
+from datetime import datetime
+
+from model.users import User
+
+# Change variable name and API name and prefix
+players_api = Blueprint('players_api', __name__,
+                   url_prefix='/api/players')
+
+# API docs https://flask-restful.readthedocs.io/en/latest/api.html
+api = Api(players_api)
+
+class UserAPI:        
+    class _Create(Resource):
+        def post(self):
+            ''' Read data for json body '''
+            body = request.get_json()
+            
+            ''' Avoid garbage in, error checking '''
+            # validate name
+            name = body.get('name')
+            if name is None or len(name) < 2:
+                return {'message': f'Name is missing, or is less than 2 characters'}, 210
+            # validate uid
+            uid = body.get('uid')
+            if uid is None or len(uid) < 2:
+                return {'message': f'User ID is missing, or is less than 2 characters'}, 210
+            # look for password and dob
+            password = body.get('password')
+            tokens = body.get('tokens')
+
+            ''' #1: Key code block, setup USER OBJECT '''
+            uo = User(name=name, 
+                      uid=uid)
+            
+            ''' Additional garbage error checking '''
+            # set password if provided
+            if password is not None:
+                uo.set_password(password)
+            # convert to date type
+            
+            
+            ''' #2: Key Code block to add user to database '''
+            # create user in database
+            player = uo.create()
+            # success returns json of user
+            if player:
+                return jsonify(player.read())
+            # failure returns error
+            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 210
+
+    class _Read(Resource):
+        def get(self):
+            users = User.query.all()    # read/extract all users from database
+            json_ready = [user.read() for user in users]  # prepare output in json
+            return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+
+    # building RESTapi endpoint
+    api.add_resource(_Create, '/create')
+    api.add_resource(_Read, '/')
