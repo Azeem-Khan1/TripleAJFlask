643963030b6055c84c2fb3590d67859b2dd4415a
Azeem-Khan1 <aalookid@gmail.com>
2023-03-03 23:47:49 -0800

    cleaning up

--- main.py
+++ main.py
@@ -4,26 +4,18 @@ import threading
 from flask import render_template  # import render_template from "public" flask libraries
 
 # import "packages" from "this" project
-from __init__ import app,db  # Definitions initialization
-from model.jokes import initJokes
-from model.users import initUsers
+from __init__ import app,db  # Defines initialization
 from model.players import initPlayers
 
 
 # setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
-from api.player import player_api
+from api.player import player_api # Blueprint import api definition
 
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
 
@@ -43,8 +35,6 @@ def stub():
 @app.before_first_request
 def activate_job():  # activate these items 
     db.init_app(app)
-    initJokes()
-    initUsers()
     initPlayers()
 
 # this runs the application on the development server
